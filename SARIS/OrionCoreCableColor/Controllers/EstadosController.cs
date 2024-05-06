d823b995a286c780b2f8d45d24f8a4e3c61f2df4
isramos2005 <125476149+isramos2005@users.noreply.github.com>
2024-05-05 21:31:03 -0600

    Nuevos Catalagos

---
+++ SARIS/OrionCoreCableColor/Controllers/EstadosController.cs
@@ -0,0 +1,174 @@
+﻿using OrionCoreCableColor.DbConnection;
+using OrionCoreCableColor.Models.Estados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrionCoreCableColor.Controllers
+{
+    public class EstadosController : BaseController
+    {
+        // GET: Estados
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public JsonResult CargarListaEstados()
+        {
+            try
+            {
+                using (var context = new SARISEntities1())
+                {
+                    var jsonResult = Json(context.sp_Estados_Lista().Select(x => new ListaEstadosViewModel
+                    {
+                        fiIDEstado = x.fiIDEstado,
+                        fcDescripcionEstado = x.fcDescripcionEstado,
+                        fcClaseColor = x.fcClaseColor,
+                    
+
+                    }).ToList(), JsonRequestBehavior.AllowGet);
+                    jsonResult.MaxJsonLength = Int32.MaxValue;
+                    return jsonResult;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+
+            using (var contextSaris = new SARISEntities1())
+            {
+
+                ViewBag.Clase = new List<SelectListItem>
+                {
+                    new SelectListItem
+                    {
+                        Value = "success",
+                        Text = "success"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "warning",
+                        Text = "warning"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "danger",
+                        Text = "danger"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "info",
+                        Text = "info"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "secundary",
+                        Text = "secundary"
+                    },
+                };
+                return PartialView(new EstadosCrearViewModel());
+            }
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Crear(EstadosCrearViewModel model)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var newModel = context.sp_Estados_Insertar(model.fcDescripcionEstado, model.fcClaseColor);
+
+                return EnviarResultado(true, "Crear Estado", "Se Creó Satisfactoriamente");
+
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            using (var context = new SARISEntities1())
+            {
+
+                ViewBag.Clase = new List<SelectListItem>
+                {
+                    new SelectListItem
+                    {
+                        Value = "success",
+                        Text = "success"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "warning",
+                        Text = "warning"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "danger",
+                        Text = "danger"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "info",
+                        Text = "info"
+                    },
+                    new SelectListItem
+                    {
+                        Value = "secundary",
+                        Text = "secundary"
+                    },
+                };
+                var estado = context.sp_Estados_Lista().FirstOrDefault(x => x.fiIDEstado == id);
+                if (estado != null)
+                {
+                    ViewBag.ListaUsuarios = context.sp_Usuarios_Maestro_Lista().ToList().Select(x => new SelectListItem { Value = x.fiIDUsuario.ToString(), Text = $"{x.fcNombreCorto}  {x.fcPuesto}" }).ToList();
+
+                    return PartialView("Crear", new EstadosCrearViewModel { fiIDEstado = estado.fiIDEstado, fcDescripcionEstado = estado.fcDescripcionEstado, fcClaseColor= estado.fcClaseColor, EsEditar = true });
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EstadosCrearViewModel model)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var newModel = context.sp_Estados_Editar (model.fiIDEstado, model.fcDescripcionEstado, model.fcClaseColor);
+                var result = context.SaveChanges() > 0;
+
+                return EnviarResultado(true, "Editar Estado", "Se edito estado satisfactoriamente");
+
+            }
+
+        }
+
+
+        public ActionResult EliminarArea(int id)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var area = context.sp_Areas_Desactivar(id);
+                return EnviarResultado(true, "Eliminar Rol");
+            }
+        }
+
+    }
+}
---
+++ SARIS/OrionCoreCableColor/Models/Estados/EstadosCrearViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrionCoreCableColor.Models.Estados
+{
+    public class EstadosCrearViewModel
+    {
+        [Display(Name = "Descripción")]
+        [Required]
+        public string fcDescripcionEstado { get; set; }
+        
+        [Display(Name = "Clase")]
+        [Required]
+        public string fcClaseColor { get; set; }
+
+        [Display(Name = "Id")]
+        public int fiIDEstado { get; set; }
+
+        public bool EsEditar { get; set; }
+
+    }
+}
