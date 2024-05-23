04aa5c2b5438f67787a0fafc3f6c209eaab44510
isramos2005 <125476149+isramos2005@users.noreply.github.com>
2024-05-23 14:12:52 -0600

    Create RequerimientosCatalogoCategorias.cs

---
+++ SARIS/SARIS/DbConnection/CMContext/RequerimientosCatalogoCategorias.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrionCoreCableColor.DbConnection.CMContext
+{
+    public partial class RequerimientosCatalogoCategorias
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+
+        //public RequerimientosCatalogoCategorias()
+        //{
+        //    UsuariosPorArea = new HashSet<Usuarios_Maestro>();
+        //}
+
+
+        [Key]
+        public string fiIDCategoriaDesarrollo { get; set; }
+
+        public string fcDescripcionCategoria { get; set; }
+
+        public int fcToken { get; set; }
+
+        public int fiEstado { get; set; }
+
+
+
+    }
+}
