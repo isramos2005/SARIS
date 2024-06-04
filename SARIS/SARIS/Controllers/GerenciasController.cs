6032bb0b753a168b41f705327550a3b41d3f8067
isramos2005 <125476149+isramos2005@users.noreply.github.com>
2024-06-04 13:44:16 -0600

    Cambios Catalogos

---
+++ SARIS/SARIS/Controllers/GerenciasController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using OrionCoreCableColor.DbConnection;
+using OrionCoreCableColor.DbConnection.CMContext;
+using OrionCoreCableColor.Models.Gerencias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrionCoreCableColor.Controllers
+{
+    public class GerenciasController : BaseController
+    {
+        // GET: Gerencias
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Lista()
+        {
+            try
+            {
+                using (var context = new SARISEntities1())
+                {
+                    var jsonResult = Json(context.sp_Requerimientos_Catalogo_Generencias_Listado().Select(x => new GerenciasViewModel
+                    {
+                        fiIDGerencia = x.fiIDGerencia,
+                        fcNombreGenerencia = x.fcNombreGenerencia,
+                        fcNombreCorto =  x.fcNombreCorto,
+                        fiEstado = x.fiEstado,
+                        fcToken = x.fcToken,
+                    }).ToList(), JsonRequestBehavior.AllowGet);
+                    jsonResult.MaxJsonLength = Int32.MaxValue;
+                    return jsonResult;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+
+            using (var contextSaris = new SARISEntities1())
+            {
+                ViewBag.ListaUsuarios = contextSaris.sp_Usuarios_Maestro_Lista().ToList().Select(x => new SelectListItem { Value = x.fiIDUsuario.ToString(), Text = $"{x.fcNombreCorto} - {x.fcPuesto}" }).ToList();
+
+                return PartialView(new GerenciasViewModel());
+            }
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Crear(GerenciasViewModel model)
+        {
+            using (var context = new SARISEntities1())
+            {
+
+                var result = context.sp_Requerimientos_Catalogo_Generencias_Insertar(model.fcNombreGenerencia.Trim(), model.fiIDUsuarioResponsable).FirstOrDefault();
+
+                var success = result > 0;
+
+                return EnviarResultado(success, "Crear Gerencia", success ? "Se Creó Satisfactoriamente" : "Error al Crear ");
+
+            }
+
+
+        }
+
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var Gerencia = context.sp_Requerimientos_Catalogo_Generencias_Listado().FirstOrDefault(x => x.fiIDGerencia == id);
+                ViewBag.ListaUsuarios = context.sp_Usuarios_Maestro_Lista().ToList().Select(x => new SelectListItem
+                {
+                    Value = x.fiIDUsuario.ToString(),
+                    Text = $"{x.fcNombreCorto} - {x.fcPuesto}",
+                    Selected = x.fiIDUsuario == Gerencia.fiIDUsuarioResponsable
+                }).ToList();
+
+
+                return PartialView("Crear", new GerenciasViewModel { fiIDGerencia = Gerencia.fiIDGerencia, fcNombreGenerencia = Gerencia.fcNombreGenerencia, EsEditar = true });
+             
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(GerenciasViewModel model)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var result = context.sp_Requerimientos_Catalogo_Generencias_Editar(model.fiIDGerencia, model.fcNombreGenerencia.Trim(), model.fiIDUsuarioResponsable).FirstOrDefault();
+
+                var success = result > 0;
+
+                return EnviarResultado(success, "Editar Gerencia", success ? "Se Editó Satisfactoriamente" : "Error al editar ");
+
+
+            }
+
+        }
+
+        public ActionResult EliminarGerencia(int id)
+        {
+            using (var context = new SARISEntities1())
+            {
+                var result = context.sp_Requerimientos_Catalogo_Generencias_Eliminar(id).FirstOrDefault();
+                var success = result > 0;
+
+                return EnviarResultado(success, "Eliminar Gerencia", success ? "Se Eliminó Satisfactoriamente" : "Error al eliminar");
+            }
+        }
+
+    }
+}
---
+++ SARIS/SARIS/Models/Gerencias/GerenciasViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrionCoreCableColor.Models.Gerencias
+{
+    public class GerenciasViewModel
+    {
+        [Display(Name = "ID")]
+        [Required]
+        public int fiIDGerencia { get; set; }
+
+        [Display(Name = "Usuario Asignado")]
+        [Required]
+        public int fiIDUsuarioResponsable { get; set; }
+
+
+        [Display(Name = "Descripción")]
+        [Required]
+        public string fcNombreGenerencia { get; set; }
+
+
+        [Display(Name = "Usuario Asignado")]
+        [Required]
+        public string fcNombreCorto { get; set; }
+
+
+        [Display(Name = "Token")]
+        public string fcToken { get; set; }
+
+        [Display(Name = "Activo")]
+
+        public bool fiEstado { get; set; }
+
+        public bool EsEditar { get; set; }
+    }
+}
